827943a60862ce794d265be382bddcc83b9069b4
Viktoriia Nistorika <vnistorika@gmail.com>
2022-08-05 15:59:40 +0300

    final

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,87 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import { Component } from "react"
+import { nanoid } from 'nanoid';
+import ContactForm from './ContactForm/ContactForm';
+import ContactList from './ContactList/ContactList';
+import Filter from './Filter/Filter';
+
+export class App extends Component {
+  state = {
+    contacts: [
+      // { id: nanoid(), name: 'Rosie Simpson', number: '459-12-56' },
+      // { id: nanoid(), name: 'Hermione Kline', number: '443-89-12' },
+      // { id: nanoid(), name: 'Eden Clements', number: '645-17-79' },
+      // { id: nanoid(), name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+    name: '',
+    number: '',
+  };
+
+  addContact = ({ name, number }) => {
+    const normalizedName = name.toLowerCase();
+
+    let haveContact = false;
+    this.state.contacts.forEach(el => {
+      if (el.name.toLowerCase() === normalizedName) {
+        alert(`${name} is already in contacts`);
+        haveContact = true;
+      }
+    });
+
+    if (haveContact) {
+      return;
+    }
+
+    const contact = {
+      id: nanoid(),
+      name: name,
+      number: number,
+    };
+
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, contact],
+    }));
+  };
+
+
+  getSearchingContacts = () => {
+    const lowerNorm = this.state.filter.toLowerCase();
+    return this.state.contacts.filter(contact => contact.name.toLowerCase().includes(lowerNorm));
+  };
+
+  handleChange = evt => {
+    this.setState({ filter: evt.currentTarget.value });
+  };
+
+
+  deleteContact = id =>
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== id),
+    }));
+
+    componentDidMount (){
+      const localContacts = localStorage.getItem('contacts');
+      const parseContacts = JSON.parse(localContacts)
+
+      if (parseContacts) {
+        this.setState({contacts : parseContacts})
+      }
+    }
+
+    componentDidUpdate (prevProps, prevState) {
+      if (this.state.contacts !== prevState.contacts){
+        localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
+      }
+    }
+
+  render(){
+    return (
+      <div className="div">
+        <ContactForm onSubmit={this.addContact}/>
+        <h2 className="contactsTitle">Contacts</h2>
+        <ContactList contacts={this.getSearchingContacts()} deleteContact={this.deleteContact}/>
+        <Filter filter={this.state.filter} handleChange={this.handleChange}/>
+      </div>
+    );
+  }
 };
---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import s from './ContactForm.module.css';
+
+class ContactForm extends Component {
+
+  state = {
+    name: '',
+    number: '',
+  };
+
+  handleChange = evt => {
+    const { name, value } = evt.target;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = evt => {
+    evt.preventDefault();
+
+    const { onSubmit } = this.props;
+    onSubmit(this.state);
+    this.resetForm();
+  };
+
+  resetForm = () => {
+    this.setState(() => ({
+      name: '',
+      number: '',
+    }));
+  };
+
+  render() {
+    return (
+      <form  onSubmit={this.handleSubmit}>
+        <h1 className={s.title}>Phonebook</h1>
+       <div className={s.thumb}>
+       <label className={s.label}>
+          Name
+          <input
+            className={s.inp}
+            value={this.state.name}
+            onChange={this.handleChange}
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+        </label>
+
+        <label className={s.label}>
+          Number
+          <input
+            className={s.inp}
+            value={this.state.number}
+            onChange={this.handleChange}
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+          />
+        </label>
+
+       </div>
+        <button type="submit" className={s.btn}>
+          Add contact
+        </button>
+      </form>
+    );
+  }
+}
+export default ContactForm;
